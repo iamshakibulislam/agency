dd08824ce057512d0c48cad5c80474c1c3191cac
shakibul islam <iamshakibulislam@gmail.com>
2021-07-15 16:24:59 +0600

    added db

--- agency/settings.py
+++ agency/settings.py
@@ -73,12 +73,31 @@ WSGI_APPLICATION = 'agency.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+'''
+
+
+#production db
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webheagf_web-agency',
+        'USER': 'webheagf_shakil',
+        'PASSWORD': 'Shakil121@',
+        'HOST': 'localhost',}
+       
+}
+
 
 
 # Password validation
